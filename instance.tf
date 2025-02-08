9eba0ceb4a92b5ee9f4c3efff95f2fd2bec6a94f
srisaivarmassv <srisaivarmassv595@gmail.com>
2025-02-08 13:01:36 +0530

    added security groups

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_1.id
-  
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
   
   associate_public_ip_address = true
 
---
+++ security_groups.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  security_group_id = aws_security_group.nginx_sg.id
+  type             = "ingress"
+  from_port        = 80
+  to_port          = 80
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  security_group_id = aws_security_group.nginx_sg.id
+  type             = "ingress"
+  from_port        = 22
+  to_port          = 22
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_outbound" {
+  security_group_id = aws_security_group.nginx_sg.id
+  type             = "egress"
+  from_port        = 0
+  to_port          = 0
+  protocol        = "-1"
+  cidr_blocks     = ["0.0.0.0/0"]
+}
